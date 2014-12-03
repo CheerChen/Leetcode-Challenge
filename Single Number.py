e3b4643cdb6f9ba651004f9a33c6643aa29b9059
Luke Chen <cheerchen.37@gmail.com>
2014-12-04 01:37:40 +0800

    update
    Single Number

--- Single Number.py
+++ Single Number.py
@@ -1,1 +1,22 @@
-#此处应有代码
+#Single Number
+#问题描述
+#Given an array of integers, every element appears twice except for one. Find that single one.
+#找出两两重复的数组中单身的一个
+#我的做法:先排序，两两比较大小，不相等的即为单个
+#最快方法：异或
+
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        if len(A)==1:
+            return A[0]
+        else:
+            A=sorted(A)
+            l=len(A)
+            i=0
+            while i<l:
+                if i<l-1 and A[i]==A[i+1]:
+                    i=i+2
+                else:
+                    return A[i]
