50857349ace352ace3b0b1bb8f6d84eb1facd398
cheerchen <cheerchen37@gmail.com>
2015-02-06 16:00:40 +0800

    done

--- Largest Number.py
+++ Largest Number.py
@@ -1,10 +1,43 @@
+#Largest Number
+#问题描述
+#Given a list of non negative integers, arrange them such that they form the largest number.
+#得到一个列表中的数字所能组合成的最大数字
+#思路:自定义比较函数，逐个字符对比，位数不够就拼自己再做对比
+#注意得到结果要先转成数字
+#用全排列肯定慢啊
+
 class Solution:
     # @param num, a list of integers
     # @return a string
+    #57ms
     def largestNumber(self, num):
-        strs=[]
-        for x in num:
-            strs.append(str(x))
+        strs=map(str,num)
+        if len(strs)>1:
+            strs.sort(self.compare,reverse = True)
+        
+        ret=int(''.join(strs))
+        return str(ret)
+
+    def compare(self,x,y):
+        p=0
+        while True:
+            if p==len(x):
+                x+=x
+            if p==len(y):
+                y+=y
+            if x==y:
+                return 0
+            if x[p]>y[p]:
+                return 1
+            elif x[p]==y[p]:
+                p+=1
+            else:
+                return -1
+
+
+    # Memory Limit Exceeded
+    def largestNumberSlow(self, num):
+        strs=map(str,num)
         if len(num)>1:
             strs=sorted(strs,reverse=True)
             ret=''
@@ -29,16 +62,6 @@ class Solution:
 
         return ''.join(strs)
         
-    def commonPrefix(self,s1,s2):
-        p=1
-        ret=False
-        if s1==s2:
-            return s1
-        while s1[0:p]==s2[0:p]:
-            ret=s1[0:p]
-            p+=1
-        return ret
-        
     def perm(self,l):
         if(len(l)<=1):
             return [l]
@@ -60,4 +83,3 @@ class Solution:
         
 a=Solution()
 print a.largestNumber([12,123,1212])
-# print a.arrayReplace(12,0,[121,12,12])
